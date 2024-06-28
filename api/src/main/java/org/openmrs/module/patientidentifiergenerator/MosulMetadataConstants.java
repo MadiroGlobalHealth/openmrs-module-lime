419dc02f7c312936dd6f4c2cf1f50d469a5a52f8
jnsereko <nserekojowashi@gmail.com>
2024-06-28 14:24:46 +0300

    Merge adding metadata module with idgen

---
+++ api/src/main/java/org/openmrs/module/patientidentifiergenerator/MosulMetadataConstants.java
@@ -0,0 +1,37 @@
+package org.openmrs.module.patientidentifiergenerator;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MosulMetadataConstants {
+
+    /**x
+     * Users Names
+     */
+    public static final String REGISTRATION_OFFICER_USERNAME = "REG_officer";
+    public static final String MENTAL_HEALTH_NURNSE_USERNAME = "MH_nurse";
+    public static final String MENTAL_HEALTH_COUNSELOR_USERNAME = "MH_counselor";
+    public static final String MENTAL_HEALTH_DOCTOR_USERNAME = "MH_doctor";
+    public static final String MATERNITY_NURSE_USERNAME = "MAT_nurse";
+
+    /**x
+     * Roles Names
+     */
+    public static final String REGISTRATION_OFFICER_ROLE_NAME = "Registration Officer";
+    public static final String MENTAL_HEALTH_NURNSE_ROLE_NAME = "Mental Health Nurse";
+    public static final String MENTAL_HEALTH_COUNSELOR_ROLE_NAME = "Mental Health Counselor";
+    public static final String MENTAL_HEALTH_DOCTOR_ROLE_NAME = "Mental Health Doctor";
+    public static final String MATERNITY_NURSE_ROLE_NAME = "Maternity Nurse";
+
+    /**x
+     * usernames and there corresponding role names
+     */
+    public static final Map<String, String> USER_ROLES = Collections.unmodifiableMap(new HashMap<String, String>() {{
+        put(REGISTRATION_OFFICER_USERNAME, REGISTRATION_OFFICER_ROLE_NAME);
+        put(MENTAL_HEALTH_NURNSE_USERNAME, MENTAL_HEALTH_NURNSE_ROLE_NAME);
+        put(MENTAL_HEALTH_COUNSELOR_USERNAME, MENTAL_HEALTH_COUNSELOR_ROLE_NAME);
+        put(MENTAL_HEALTH_DOCTOR_USERNAME, MENTAL_HEALTH_DOCTOR_ROLE_NAME);
+        put(MATERNITY_NURSE_USERNAME, MATERNITY_NURSE_ROLE_NAME);
+    }});
+} 
---
+++ api/src/main/java/org/openmrs/module/patientidentifiergenerator/MosulMetadataUserListener.java
@@ -0,0 +1,48 @@
+package org.openmrs.module.patientidentifiergenerator;
+
+import org.openmrs.Role;
+import org.openmrs.User;
+import org.openmrs.UserSessionListener;
+import org.openmrs.api.context.Context;
+import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Implementation of UserSessionListener hence adding roles to the corresponding
+ * roles
+ */
+@Component
+public class MosulMetadataUserListener implements UserSessionListener {
+
+	private static final Logger log = LoggerFactory.getLogger(MosulMetadataUserListener.class);
+
+    @Override
+    public void loggedInOrOut(User user, Event event, Status status) {
+        try {
+            if (event == Event.LOGIN) {
+                String mosulUsername = user.getUsername();
+                String mosulRoleName = MosulMetadataConstants.USER_ROLES.get(mosulUsername);
+
+                if (mosulRoleName != null) {
+                    Role mosulRole = Context.getUserService().getRole(mosulRoleName);
+                    verifyUserRole(user, mosulRole);
+                } else {
+                    log.debug("user: '{}' is not a Mosul user hence skipping adding Mosul role", user.getUsername());
+                }
+            }
+        } catch (Exception e) {
+            log.error("Unable to assign Mosul user Corresponding Role. ", e);
+        }
+    }
+
+    private void verifyUserRole(User user, Role role) {
+        if (!user.hasRole(role.getRole())) {
+            user.addRole(role);
+            Context.getUserService().saveUser(user);
+            log.debug("Granded Mosul user: '{}' Mosul role: '{}'", user.getUsername(), role.getRole());
+        } else {
+            log.debug("Mosul user: '{}' already has Mosul role: '{}''. Skipping...", user.getUsername(), role.getRole());
+        }
+    }
+}
